d28c1e9e25e5ef7cac202e37a15f896c62b8d7b1
Arne Schauf <git@as-wesbservices.de>
2014-04-11 02:44:33 +0200

    fix negativ index problem

--- nsupdate/stats/views.py
+++ nsupdate/stats/views.py
@@ -8,6 +8,6 @@ class StatsView(TemplateView):
         context = super(StatsView, self).get_context_data(**kwargs)
         context['user_counts'] = StatisticsEntry.objects.filter(stat_type='user_count').order_by('created')[:30]
         context['host_counts'] = StatisticsEntry.objects.filter(stat_type='host_count').order_by('created')[:30]
-        context['ip_update_counts'] = StatisticsEntry.objects.filter(stat_type='ip_update_count').order_by('created')[-31:-1]
+        context['ip_update_counts'] = list(StatisticsEntry.objects.filter(stat_type='ip_update_count').order_by('created'))[-31:-1]
         context['nav_stats'] = True
         return context
