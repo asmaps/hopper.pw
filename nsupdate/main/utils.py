bc8f2dfc5e879851c960ca6c85cada31a6cda704
Arne Schauf <git@as-webservices.de>
2014-07-03 01:15:45 +0200

    strip ipaddress

--- nsupdate/main/utils.py
+++ nsupdate/main/utils.py
@@ -3,5 +3,5 @@
 def get_remote_addr(request):
     addr = request.META.get('REMOTE_ADDR')
     if not addr:
-        addr = request.META.get('HTTP_X_FORWARDED_FOR').split(',')[-1:][0]
+        addr = request.META.get('HTTP_X_FORWARDED_FOR').split(',')[-1:][0].strip()
     return addr
