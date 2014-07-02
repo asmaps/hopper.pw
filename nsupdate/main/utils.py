9fa6d97bef381eb00d294a43b2f2dde538038dbe
Arne Schauf <git@as-webservices.de>
2014-07-03 01:13:12 +0200

    add missing file

---
+++ nsupdate/main/utils.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+
+def get_remote_addr(request):
+    addr = request.META.get('REMOTE_ADDR')
+    if not addr:
+        addr = request.META.get('HTTP_X_FORWARDED_FOR').split(',')[-1:][0]
+    return addr
