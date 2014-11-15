695b63cc6bf6ed86ce492fd95ed6c9d30fefc55a
Kalle Bronsen <bronsen@nrrd.de>
2014-11-15 14:01:16 +0100

    rename sqlite file to reflect project name

--- hopperpw/hopperpw/settings.py
+++ hopperpw/hopperpw/settings.py
@@ -18,7 +18,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'hopper.sqlite',                      # Or path to database file if using sqlite3.
+        'NAME': 'hopperpw.sqlite',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
