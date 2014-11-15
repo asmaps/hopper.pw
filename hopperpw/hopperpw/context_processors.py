5978c90bdb5012874b7819b8195921cc966cbc02
Kalle Bronsen <bronsen@nrrd.de>
2014-11-15 16:04:01 +0100

    simplify dict construction

--- hopperpw/hopperpw/context_processors.py
+++ hopperpw/hopperpw/context_processors.py
@@ -4,9 +4,10 @@ from django.contrib.sites.models import Site
 
 
 def add_settings(request):
-    context = {}
-    context['WWW_IPV4_HOST'] = settings.WWW_IPV4_HOST
-    context['WWW_IPV6_HOST'] = settings.WWW_IPV6_HOST
+    context = {
+        'WWW_IPV4_HOST': settings.WWW_IPV4_HOST,
+        'WWW_IPV6_HOST': settings.WWW_IPV6_HOST,
+    }
     try:
         context['ENABLE_TRACKING'] = settings.ENABLE_TRACKING
     except:
