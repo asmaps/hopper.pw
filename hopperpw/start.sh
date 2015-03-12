07ce02d94444deff71df9ca02ea68c3736308929
Arne Schauf <git@as-webservices.de>
2015-03-12 23:46:02 +0100

    fix python 3 -> 2

--- hopperpw/start.sh
+++ hopperpw/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python3 manage.py migrate --noinput
+python manage.py migrate --noinput
 if [ "$1" = 'worker' ]; then
     celery -n hopperpw.%h -A hopperpw worker -B --loglevel=info
 else
-    python3 manage.py collectstatic --noinput
+    python manage.py collectstatic --noinput
     gunicorn hopperpw.wsgi:application --log-level=info -b "0.0.0.0:8000"
 fi
