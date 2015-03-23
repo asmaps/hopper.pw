2325c43e62c7432dccefa9b8b1ecb683dc79f811
Arne Schauf <git@as-webservices.de>
2015-03-23 23:40:46 +0100

    generate start.sh with salt on deployment

--- hopperpw/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python manage.py migrate --noinput
-if [ "$1" = 'worker' ]; then
-    celery -n hopperpw.%h -A hopperpw worker -B --loglevel=info
-else
-    python manage.py collectstatic --noinput
-    gunicorn hopperpw.wsgi:application --log-level=info -b "0.0.0.0:8000"
-fi
